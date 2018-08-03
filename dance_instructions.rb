f98d0fd31d900ce9a70c6013027d8ccaae9929a4
serendipidoussophia <scurranmoore@gmail.com>
2018-08-03 15:35:31 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,5 +1,7 @@
 def lower_body
+  def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finishing with a small skip-step backward"
